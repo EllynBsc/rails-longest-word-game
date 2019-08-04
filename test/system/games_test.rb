89574a30e24a939747a911ad61afb2d92590fc91
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2019-08-04 17:11:02 +0200

    finished

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -6,4 +6,13 @@ class GamesTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Game"
   end
+
+
+   test "Trying to enter a word" do
+    visit new_url
+    fill_in "user_letters", with: "Hello"
+    click_on "play"
+
+    assert_text "Congratulations, Hello is a valid english word"
+  end
 end
